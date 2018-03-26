e4e4a8eeff1f7ba533fc7b256ddf986e3d821be4
Taylor <amozartea@gmail.com>
2018-03-26 17:57:02 +0800

    test commit

---
+++ tests/TestBinarySearch.php
@@ -0,0 +1,12 @@
+<?php
+include __DIR__. '/bootstrap.php';
+
+use Taylor\Math\BinarySearch;
+use function Taylor\Math\biSearch;
+
+$array = array(1, 2, 3, 4, 5);
+
+//echo BinarySearch::binSearch(3, $array);
+echo "\r\n";
+echo biSearch(3, $array);
+echo "\r\n";
