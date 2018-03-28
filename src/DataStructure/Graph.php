183f0676705b4585e02b82aef3d8dbcf9909a5d8
Taylor <amozartea@gmail.com>
2018-03-28 14:16:30 +0800

    add Graph Class: BFT and DFT

--- src/DataStructure/Graph.php
+++ src/DataStructure/Graph.php
@@ -4,36 +4,56 @@ namespace Taylor\DataStructure;
 
 class Graph
 {
-    // 2D array, 0 or null: not linked 1: linked, undirected.
-    public $linkMatrix;
+    public $linkMatrix;// 2D array, 0 or null: not linked 1: linked, undirected.
     public $nodeArray;// 1D array, Weight or Value
-    private $visited; // 1D array
+    private $visited; // 1D array, 0: not visited, 1: visited
     private $itineraryNode;   // travelled ordered result, present By Node Number
     private $itineraryValue; // travelled ordered result, present By Weight or Value
 
-    public function showTrip()
+    public function showVisited():void
+    {
+        print_r($this->visited);
+    }
+
+    public function getItineraryNode():array
+    {
+        return $this->itineraryNode;
+    }
+
+    public function showTravel():void
     {
         print_r($this->itineraryNode);
     }
-    public function returnTrip()
-    {
-        return $itineraryNode;
-    }
+    
 
-    public function __construct(int $numOfNodes)
+    public function setMatrix($inputMatrix):void
     {
-        $this->reset($numOfNodes);
-    }
-    private function reset(int $n)
-    {
-        $this->itineraryNode = array();
-        $this->visited =array();
-        $this->itineraryValue = array();
-        for ($i = 0; $i < $n; $i++) {
-            $this->visited[$i] = 0;
+        $this->linkMatrix = $inputMatrix;
+        foreach ($inputMatrix as $key => $value) {
+            $this->visited[$key] = 0;
         }
     }
 
+
+    public function returnTrip()
+    {
+        return $this->itineraryNode;
+    }
+
+    public function __construct()
+    {
+        $this->itineraryNode = array();
+        $this->itineraryValue = array();
+    }
+
+    public function reset()
+    {
+        $this->itineraryNode = array();
+        $this->itineraryValue = array();
+        foreach ($this->visited as $key => $value) {
+            $this->visited[$key] = 0;
+        }
+    }
 
     public function dFT(int $startNode)
     {
@@ -52,5 +72,26 @@ class Graph
 
     public function bFT(int $startNode)
     {
+        $currentNode = $startNode;
+        $targetQueue = new Queue();
+        while (true) {
+            if ($this->visited[$currentNode]===0) {
+                $this->visited[$currentNode] = 1;
+                array_push($this->itineraryNode, $currentNode);
+                foreach ($this->linkMatrix[$currentNode] as $key => $value) {
+                    if ($value >0) { // link exists
+                        $neighbor = $key;
+                        if ($this->visited[$neighbor] === 0) {//unvisited
+                            $targetQueue->push($neighbor);
+                        }
+                    }
+                }
+            }
+            if ($targetQueue->isEmpty()) {
+                break;
+            }
+
+            $currentNode = $targetQueue->pop();
+        }
     }
 }
